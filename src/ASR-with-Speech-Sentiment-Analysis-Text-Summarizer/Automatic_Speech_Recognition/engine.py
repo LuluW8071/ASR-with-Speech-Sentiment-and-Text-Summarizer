5849b6b651b22d7240d5cca305601b6ad6d0143b
luluw <shashinmhrzn@gmail.com>
2024-09-18 17:10:13 +0545

    :rocket: [Update] Conformer Inference Script and Jit Trace

--- src/ASR-with-Speech-Sentiment-Analysis-Text-Summarizer/Automatic_Speech_Recognition/engine.py
+++ src/ASR-with-Speech-Sentiment-Analysis-Text-Summarizer/Automatic_Speech_Recognition/engine.py
@@ -14,7 +14,6 @@ from threading import Event
 
 class Listener:
     def __init__(self, sample_rate=16000, record_seconds=2):
-        """ Initializes the Listener object with specified parameters """
         self.chunk = 1024
         self.sample_rate = sample_rate
         self.record_seconds = record_seconds
@@ -27,92 +26,61 @@ class Listener:
                                   frames_per_buffer=self.chunk)
 
     def listen(self, queue):
-        """
-        Continuously listens for audio input and appends it to the provided queue.
-
-        Args:
-            queue (list): List to store audio input data.
-        """
         while True:
-            data = self.stream.read(self.chunk , exception_on_overflow=False)
+            data = self.stream.read(self.chunk, exception_on_overflow=False)
             queue.append(data)
             time.sleep(0.01)
 
     def run(self, queue):
-        """ Starts a new thread to listen for audio input """
         thread = threading.Thread(target=self.listen, args=(queue,), daemon=True)
         thread.start()
         print("\nSpeech Recognition engine is now listening... \n")
 
 
 class SpeechRecognitionEngine:
-    def __init__(self, model_file, ken_lm_file, context_length=10):
-        """
-        Initializes the SpeechRecognitionEngine with the specified parameters.
-
-        Args:
-            model_file (str): Path to the optimized model file.
-            ken_lm_file (str): Path to the KenLM language model file.
-            context_length (int): Length of the context window in seconds (default: 10 seconds).
-        """
+    def __init__(self, model_file, ken_lm_file=None, context_length=10):
         self.listener = Listener(sample_rate=16000)
         self.model = torch.jit.load(model_file)
-        self.model.eval().to('cpu')  # Run on cpu
+        self.model.eval().to('cpu')
         self.featurizer = get_featurizer(16000)
         self.audio_q = list()
-        self.hidden = (torch.zeros(4, 1, 1024), torch.zeros(4, 1, 1024))        # num_layers*2, 1, hidden_size
-        self.beam_results = ""
         self.out_args = None
         self.beam_search = CTCBeamDecoder(beam_size=100, kenlm_path=ken_lm_file)
-        self.context_length = context_length * 50                               # multiply by 50 because each 50 from output frame is 1 second
-        self.start = False
+        self.context_length = context_length * 50
 
     def save(self, waveforms, fname="audio_temp"):
-        """
-        Saves audio waveforms to a WAV file.
-
-        Args:
-            waveforms (list): List of audio waveforms.
-            fname (str): File name for the saved WAV file (default: "audio_temp").
-
-        Returns:
-            str: File path of the saved WAV file.
-        """
         wf = wave.open(fname, "wb")
-
-        wf.setnchannels(1)                                                  # Mono Channel
-        wf.setsampwidth(self.listener.p.get_sample_size(pyaudio.paInt16))   # Sample format: Int16
-        wf.setframerate(16000)                                              # Sample Rate
+        wf.setnchannels(1)
+        wf.setsampwidth(self.listener.p.get_sample_size(pyaudio.paInt16))
+        wf.setframerate(16000)
         wf.writeframes(b"".join(waveforms))
         wf.close()
         return fname
 
     def predict(self, audio):
-        """
-        Performs speech recognition on the provided audio data.
-
-        Args:
-            audio (list): List of audio waveforms.
-
-        Returns:
-            tuple: A tuple containing the recognized text and the current context length in seconds.
-        """
         with torch.inference_mode():
             fname = self.save(audio)
-            waveform, _ = torchaudio.load(fname)                            # Don't normalize on train
-            log_mel = self.featurizer(waveform).unsqueeze(1)
-            out, self.hidden = self.model(log_mel, self.hidden)
-            out = torch.nn.functional.softmax(out, dim=2)
-            out = out.transpose(0, 1)
+            waveform, _ = torchaudio.load(fname)
+            log_mel = self.featurizer(waveform).transpose(1, 2)
+            # print("Log Mel Shape", log_mel.shape)
+            encoder_out = self.model.encoder(log_mel)
+            # print("Encoder Shape", encoder_out.shape)
+            decoder_out = self.model.decoder(encoder_out)
+            # print("Decoder Shape", decoder_out.shape)
+
+            out = torch.nn.functional.softmax(decoder_out, dim=-1)
+            # print("Output Shape", out.shape)
+
             self.out_args = out if self.out_args is None else torch.cat((self.out_args, out), dim=1)
+            # print("Self Args Shape", self.out_args.shape)
+
             results = self.beam_search(self.out_args)
-            current_context_length = self.out_args.shape[1] / 50            # in seconds
+            current_context_length = self.out_args.shape[1] / 50
             if self.out_args.shape[1] > self.context_length:
                 self.out_args = None
             return results, current_context_length
 
     def inference_loop(self, action):
-        """ Continuously performs speech recognition on the audio queue """
         while True:
             if len(self.audio_q) < 5:
                 continue
@@ -123,47 +91,34 @@ class SpeechRecognitionEngine:
             time.sleep(0.05)
 
     def run(self, action):
-        """ Starts the speech recognition engine """
         self.listener.run(self.audio_q)
-        thread = threading.Thread(target=self.inference_loop,
-                                    args=(action,), daemon=True)
+        thread = threading.Thread(target=self.inference_loop, args=(action,), daemon=True)
         thread.start()
 
 
 class DemoAction:
-    """ Class to handle the action after performing speech recognition """
     def __init__(self):
         self.asr_results = ""
         self.current_beam = ""
 
     def __call__(self, x):
-        """ Combines the current and previous results and prints them """
-        # Unpack the tuple
         results, current_context_length = x
-
-        # Combine the current beam results with previous results and print transcript
         self.current_beam = results
         transcript = " ".join(self.asr_results.split() + results.split())
         print(transcript)
-
-        # Update the current transcript
         if current_context_length > 10:
             self.asr_results = transcript
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="demoing the speech recognition engine in terminal.")
-    parser.add_argument('--model_file', type=str, default=None, required=True,
-                        help='optimized file to load. use freeze.py')
-    parser.add_argument('--ken_lm_file', type=str, default=None, required=False,
-                        help='If you have an ngram lm use to decode')
+    parser = argparse.ArgumentParser(description="Demoing the speech recognition engine.")
+    parser.add_argument('--model_file', type=str, required=True, help='Path to the optimized Conformer model file.')
+    parser.add_argument('--kenlm_file', type=str, help='Path to the KenLM file (optional).')
 
     args = parser.parse_args()
 
-    # Activate speech recognition engine
-    asr_engine = SpeechRecognitionEngine(args.model_file, args.ken_lm_file)
+    asr_engine = SpeechRecognitionEngine(args.model_file, args.kenlm_file)
     action = DemoAction()
 
-    # Start the speech recognition engine 
-    # and wait for the threading event to keep the program running
     asr_engine.run(action)
     threading.Event().wait()
